cdeb76fcd77c080768f940e4a42cc9a8ab5cebd7
MrBE <essienedim@gmail.com>
2021-04-04 16:33:40 +0100

    chore: set up network objects

--- app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
@@ -6,7 +6,7 @@ class CivicsHttpClient: OkHttpClient() {
 
     companion object {
 
-        const val API_KEY = "" //TODO: Place your API Key Here
+        const val API_KEY = "AIzaSyDzLrtfG87QI12obqBnoqTPZApTNVEDZs4" //COMPLETED: Place your API Key Here
 
         fun getClient(): OkHttpClient {
             return Builder()
